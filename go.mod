7469d26fdc78934fbe60af19139d5cc583d022d7
mataperezluis <mata.perez.luis@gmail.com>
2020-09-25 22:26:38 -0300

    prueba

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/mataperezluis/goGraphql
+
+go 1.15
+
+require (
+	github.com/99designs/gqlgen v0.13.0
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/golang-migrate/migrate/v4 v4.12.2 // indirect
+	github.com/vektah/gqlparser/v2 v2.1.0
+)
---
+++ server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/mataperezluis/goGraphql/graph"
+	"github.com/mataperezluis/goGraphql/graph/generated"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+    router := chi.NewRouter()
+database.InitDB()
+	database.Migrate()
+	server := handler.GraphQL(hackernews.NewExecutableSchema(hackernews.Config{Resolvers: &hackernews.Resolver{}}))
+	router.Handle("/", handler.Playground("GraphQL playground", "/query"))
+	router.Handle("/query", server)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
