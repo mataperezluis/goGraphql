2f781473f2314fab66b4bbc739ab1bbd9a5cca5c
mataperezluis <mata.perez.luis@gmail.com>
2020-09-26 10:35:08 -0300

    add links

--- internal/users/users.go
+++ internal/users/users.go
@@ -57,4 +57,30 @@ func GetUserIdByUsername(username string) (int, error) {
 
 	return Id, nil
 }
+func (user *User) Authenticate() bool {
+	statement, err := database.Db.Prepare("select Password from Users WHERE Username = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	row := statement.QueryRow(user.Username)
+
+	var hashedPassword string
+	err = row.Scan(&hashedPassword)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return false
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	return CheckPasswordHash(user.Password, hashedPassword)
+}
+
+//CheckPassword hash compares raw password with it's hashed values
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
 
